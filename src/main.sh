6c64555ecfd0edbfb6ef1ce629a4d3fde286ebca
gonzalo <gonzalosantos89@gmail.com>
2020-08-31 20:05:08 +0200

    Move deploy script

--- src/main.sh
+++ src/main.sh
@@ -3,17 +3,22 @@
 source "$HOME_DIR/src/ensure.sh"
 source "$HOME_DIR/src/format.sh"
 
-
 main() {
   ensure::env_variable_exist "INPUT_SSH_USER"
   ensure::env_variable_exist "INPUT_SSH_HOST"
   ensure::env_variable_exist "INPUT_SSH_PORT"
+  ensure::env_variable_exist "INPUT_SCRIPT"
+
+  if [[ -z "$INPUT_SSH_HOST" ]]; then
+    format::echoTitle "Building"
+    exit 0
+  fi
+
+  format::echoTitle "Prepare deploy script"
+
+  echo $INPUT_SCRIPT | cat > $HOME_DIR/src/deploy.sh
 
   format::echoTitle "Provisioning virtual machine..."
-
-  #if [[ -z "$INPUT_SSH_HOST" ]]; then
-  #  exit 0
-  #fi
 
   if [[ ! -z "$INPUT_SSH_KEY" ]]; then
     ensure::env_variable_exist "INPUT_SSH_KEY"
