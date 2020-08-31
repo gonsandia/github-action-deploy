e5dd20e32c6e77d7256117e91992a118b960d9cc
gonzalo <gonzalosantos89@gmail.com>
2020-08-31 21:52:44 +0200

    Move deploy script

--- src/main.sh
+++ src/main.sh
@@ -14,9 +14,10 @@ main() {
     exit 0
   fi
 
-#  format::echoTitle "Prepare deploy script"
-#
-#  echo $INPUT_SSH_SCRIPT | cat > $HOME_DIR/src/deploy.sh
+  format::echoTitle "Prepare deploy script"
+  echo -e $INPUT_SSH_SCRIPT | cat > $HOME_DIR/src/deploy.sh
+
+  cat $HOME_DIR/src/deploy.sh
 
   format::echoTitle "Provisioning virtual machine..."
 
@@ -26,13 +27,13 @@ main() {
     mkdir -p ~/.ssh/
     echo "$INPUT_SSH_KEY" > ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
-    echo $INPUT_SSH_SCRIPT | ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $SSH_USER@$SSH_HOST -p $INPUT_SSH_PORT
+    cat $HOME_DIR/src/deploy.sh | ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $SSH_USER@$SSH_HOST -p $INPUT_SSH_PORT
   else
     ensure::env_variable_exist "INPUT_SSH_PASSWORD"
 
     format::echoTitle "Connecting ssh with sshpass"
 
-    echo $INPUT_SSH_SCRIPT | sshpass -p $INPUT_SSH_PASSWORD ssh -o StrictHostKeyChecking=no $INPUT_SSH_USER@$INPUT_SSH_HOST -p $INPUT_SSH_PORT
+    cat $HOME_DIR/src/deploy.sh | sshpass -p $INPUT_SSH_PASSWORD ssh -o StrictHostKeyChecking=no $INPUT_SSH_USER@$INPUT_SSH_HOST -p $INPUT_SSH_PORT
   fi
 
   exit $?
