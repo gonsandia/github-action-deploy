4a9f945cb844a9d8829b3e68d93349ac403ff63f
gonzalo <gonzalosantos89@gmail.com>
2020-08-31 21:13:57 +0200

    Move deploy script

--- src/main.sh
+++ src/main.sh
@@ -26,13 +26,13 @@ main() {
     mkdir -p ~/.ssh/
     echo "$INPUT_SSH_KEY" > ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
-    cat $INPUT_SSH_SCRIPT | ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $SSH_USER@$SSH_HOST -p $INPUT_SSH_PORT
+    echo $INPUT_SSH_SCRIPT | ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $SSH_USER@$SSH_HOST -p $INPUT_SSH_PORT
   else
     ensure::env_variable_exist "INPUT_SSH_PASSWORD"
 
     format::echoTitle "Connecting ssh with sshpass"
 
-    cat $HOME_DIR/src/deploy.sh | sshpass -p $INPUT_SSH_PASSWORD ssh -o StrictHostKeyChecking=no $INPUT_SSH_USER@$INPUT_SSH_HOST -p $INPUT_SSH_PORT
+    echo $INPUT_SSH_SCRIPT | sshpass -p $INPUT_SSH_PASSWORD ssh -o StrictHostKeyChecking=no $INPUT_SSH_USER@$INPUT_SSH_HOST -p $INPUT_SSH_PORT
   fi
 
   exit $?
